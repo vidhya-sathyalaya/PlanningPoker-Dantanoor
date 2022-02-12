8737650d8d7128ffb78fbc538c2688102a0fca13
rishikeshraj0 <rmeesal1@asu.edu>
2022-02-11 19:45:51 -0800

    display logOfPlayers

---
+++ logOfPlayers.py
@@ -0,0 +1,7 @@
+def display_log(logOfPlayer):
+    for i in logOfPlayer:
+        print(i) #TODO: Return after the logs have been added in the code
+
+
+playersLog=[] #TODO: Initialize after the logs are created
+display_log(playersLog)
