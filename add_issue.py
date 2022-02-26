d232bb98755d320e4ffce56b9afa32ee0baa4474
Vidhya Sathyalaya Ramakrishnan <vsathyal@asu.edu>
2022-02-25 20:54:47 -0700

    US57T69 Changed layout and include description i/p

--- add_issue.py
+++ add_issue.py
@@ -1,23 +1,31 @@
 from tkinter import *
+import tkinter
 
 main_screen = Tk()
 main_screen.title("ADD ISSUE")
 
+def sendIssueToServer():
+    pass
 def addIssue():
-    issue = Entry(main_screen)
-    issue.pack()
+    
+    main_screen.geometry("300x100")
+
+    Label(text="Issue Title").grid(row=0, column=0)
+    issue_name = Entry(main_screen)
+    issue_name.grid(row=0, column=1)
+    Label(text="Issue Description").grid(row=1, column=0)
+    issue_desc = Entry(main_screen)
+    issue_desc.grid(row=1, column=1)
+    Label(text="").grid(row=2)
 
     
     def showIssue():
     
-        show_issue = Label(main_screen, text=issue.get()).pack()
+        show_issue = Label(main_screen, text=issue_name.get()).grid(row=4)
 
-    submit_issue = Button(main_screen,text='Submit Issue',command=showIssue).pack()
+    submit_issue = Button(main_screen,text='Submit Issue',command=showIssue).grid(row=3, column=1)
     
-    return issue 
+    main_screen.mainloop()
 
 
-
-
-Issue_button = Button(main_screen, text = "Add Issue",command=addIssue).pack()
-main_screen.mainloop()
+addIssue()
