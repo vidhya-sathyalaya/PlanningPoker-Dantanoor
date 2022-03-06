d16bc0cba67e7d6b5210c78a492e65cf51663ca6
Subramanian <saruna11@asu.edu>
2022-03-05 23:17:05 -0700

    Sprint 4 Integrations
    Co-authored-by: Subramanian <saruna11@asu.edu>
    Co-authored-by: Vidhya <vsathyal@asu.edu>

--- showdown.py
+++ showdown.py
@@ -37,14 +37,14 @@ def display():
         Radiobutton(d, text = i, variable=judgement, value = player_list.index(i)).pack(anchor=W)
     show_description=Button(d, text="Show Description", command = lambda: open(judgement.get())).pack(pady=15)
     
-      def end():
-        consesnsus = messagebox.askquestion("Consensus","Have you reached a consensus ?" )
-        if consesnsus == "yes":
+    def end():
+        consensus = messagebox.askquestion("Consensus","Have you reached a consensus ?" )
+        if consensus == "yes":
             d.quit()
             main.quit()
         else:
             pass
-    
+
     end_round = Button(d, text = "End Round", command = end).pack()
 
     
